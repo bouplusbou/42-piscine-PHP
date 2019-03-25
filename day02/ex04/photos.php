0e7db3ca018d869d5d2a421f7117c2eb26ebde31
Baptiste BOUCHER <bboucher@e1r8p7.42.fr>
2019-03-25 16:27:35 +0100

    day02 ex04

--- day02/ex04/photos.php
+++ day02/ex04/photos.php
@@ -1,26 +1,46 @@
 #!/usr/bin/php
 <?php
-if ($argc < 2) {
-	exit();
-}
-$page = file_get_contents($argv[1]);
-preg_match_all("/<img .*?>/", $page, $tab);
-$stock = $tab[0];
-preg_match("/[^\/]*\$/", $argv[1], $folder_tab);
-$folder = $folder_tab[0] . "/";
-if (file_exists($folder)) {
-	exit ;
-}
-mkdir($folder);
-foreach ($stock as $pic) {
-	preg_match("/src=[\"'].*?[\"']/", $pic, $src_tab);
-	$src = substr($src_tab[0], 5, -1);
-	preg_match("/[^\/]*\$/", $src, $name_tab);
-	$name = $name_tab[0];
-	if (preg_match("/http?.:\/\//", $src)) {
-		copy($src, $folder . $name);
-	} else {
-		copy($argv[1] . "/" . trim($src, "/"), $folder . $name);
+if ($argc == 2) {
+	preg_match("~//(.*)~", $argv[1], $dir);
+	if (!file_exists($dir[1])) {
+		mkdir($dir[1], 0755, true);
+	}
+	$page = file_get_contents($argv[1]);
+	preg_match_all("~<img .*?>~", $page, $images);
+	foreach ($images[0] as $image) {
+		preg_match("~src=\"(.*?)\"~", $image, $src);
+		$paths[] = $src[1];
+	}
+	foreach ($paths as $path) {
+		if (preg_match("~(https?://)~", $path)) {
+			$path_parts = pathinfo($path);
+			$parsed_url = parse_url($path_parts['basename']);
+			$path_parts = pathinfo($parsed_url[path]);
+			$dir_path = $dir[1] . "/" . $path_parts[filename] . "." . $path_parts[extension];
+			$counter = 0;
+    		while (file_exists($dir_path)) {
+				$dir_path = $dir[1] . "/" . $path_parts[filename] . "_" . $counter. "." . $path_parts[extension];
+           		$counter++;
+     		}
+			file_put_contents($dir_path, file_get_contents($path));
+		} 
+		elseif ($path) {
+			if (preg_match("~^/~", $path)) {
+				$path = $argv[1] . $path;
+			} else {
+				$path = $argv[1] . "/" . $path;
+			}
+			$path_parts = pathinfo($path);
+			$parsed_url = parse_url($path_parts['basename']);
+			$path_parts = pathinfo($parsed_url[path]);
+			$dir_path = $dir[1] . "/" . $path_parts[filename] . "." . $path_parts[extension];
+			$counter = 0;
+    		while (file_exists($dir_path)) {
+				$dir_path = $dir[1] . "/" . $path_parts[filename] . "_" . $counter. "." . $path_parts[extension];
+           		$counter++;
+     		}
+			file_put_contents($dir_path, file_get_contents($path));
+		}
 	}
 }
 ?>
