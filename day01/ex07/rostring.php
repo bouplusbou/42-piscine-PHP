42f5f394acf20a47106bf177b78a83d303dd3153
Baptiste BOUCHER <bboucher@e1r8p9.42.fr>
2019-03-26 08:31:13 +0100

    day01 ex07 rostring well

--- day01/ex07/rostring.php
+++ day01/ex07/rostring.php
@@ -1,11 +1,11 @@
 #!/usr/bin/php
 <?php
 if ($argc >= 2) {
-	$split = explode(" ", trim(preg_replace('/\s+/', ' ', $argv[1])));
-	$last = array_pop($split);
-	$concat = "$last ";
-	foreach ($split as $elem)
-		$concat = $concat . "$elem ";
-	echo trim($concat) . "\n";	
+	$split = explode(" ", trim(preg_replace('/ +/', ' ', $argv[1])));
+    $nb = count($split);
+    for ($i = 1; $i < $nb; $i++) {
+        echo $split[$i] . " ";
+    }
+    echo $split[0] . "\n";
 }
 ?>
