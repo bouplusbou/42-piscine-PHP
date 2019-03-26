851d3b2cde802ca9d87b37c77fa4301122852959
Baptiste BOUCHER <bboucher@e1r8p9.42.fr>
2019-03-26 08:55:31 +0100

    day01 ex10 just change if

--- day01/ex10/do_op.php
+++ day01/ex10/do_op.php
@@ -1,7 +1,8 @@
 #!/usr/bin/php
 <?php
-if ($argc != 4)
+if ($argc != 4) {
 	exit("Incorrect Parameters\n");
+}
 $a = preg_replace('/[ \t]/', '', $argv[1]);
 $op = preg_replace('/[ \t]/', '', $argv[2]);
 $b = preg_replace('/[ \t]/', '', $argv[3]);
