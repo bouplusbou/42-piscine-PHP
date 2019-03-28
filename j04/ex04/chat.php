3361efdb38eaf66dfd39d8e544b30c96ccead4aa
Baptiste BOUCHER <bboucher@e2r6p12.42.fr>
2019-03-28 18:48:08 +0100

    push all days

--- j04/ex04/chat.php
+++ j04/ex04/chat.php
@@ -5,17 +5,17 @@ if (file_exists("../private/chat")) {
 	$fp = fopen("../private/chat", "r");
 	if (flock($fp, LOCK_SH)) { // acquière un verrou exclusif
 		$file_msg = file_get_contents("../private/chat");
+		fflush($fp);            // libère le contenu avant d'enlever le verrou
+		flock($fp, LOCK_UN);    // Enlève le verrou
+	} else {
+		echo "Impossible de verrouiller le fichier !";
+	}
 		$arr_msg = unserialize($file_msg);
 		foreach ($arr_msg as $msg) {
 			$time = new DateTime('@' . $msg['time']);
 			$time = $time->format('H:i');
 			echo "[".$time."] <b>".$msg['login']."</b>: ".$msg['msg']."<br />\n";
 		}
-		fflush($fp);            // libère le contenu avant d'enlever le verrou
-		flock($fp, LOCK_UN);    // Enlève le verrou
-	} else {
-		echo "Impossible de verrouiller le fichier !";
-	}
 	fclose($fp);
 }
 ?>
--- j04/ex04/speak.php
+++ j04/ex04/speak.php
@@ -11,18 +11,18 @@ if (isset($_POST['msg'])) {
 	if (!file_exists("../private/chat")) {
 		file_put_contents("../private/chat", "");
 	}
+	$file_msg = file_get_contents("../private/chat");
+	$arr_msg = unserialize($file_msg);
+	date_default_timezone_set("Europe/Paris");
+	$arr_msg[] = array("login" => $_SESSION['loggued_on_user'], "time" => time(), "msg" => htmlspecialchars($_POST['msg']));
+	$arr_msg_serialized = serialize($arr_msg);
 	$fp = fopen("../private/chat", "r+");
 	if (flock($fp, LOCK_EX)) { // acquière un verrou exclusif
-		$file_msg = file_get_contents("../private/chat");
-		$arr_msg = unserialize($file_msg);
-		date_default_timezone_set("Europe/Paris");
-		$arr_msg[] = array("login" => $_SESSION['loggued_on_user'], "time" => time(), "msg" => htmlspecialchars($_POST['msg']));
-		$arr_msg_serialized = serialize($arr_msg);
 		file_put_contents("../private/chat", "$arr_msg_serialized");
 		fflush($fp);            // libère le contenu avant d'enlever le verrou
 		flock($fp, LOCK_UN);    // Enlève le verrou
 	} else {
-		echo "Impossible de verrouiller le fichier !";
+		echo "Impossible de verrouiller le fichier !\n";
 	}
 	fclose($fp);
 }
