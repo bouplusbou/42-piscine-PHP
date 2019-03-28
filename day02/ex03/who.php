08f59206e0755fa472f61f3115329a277105d834
Baptiste BOUCHER <bboucher@e2r6p12.42.fr>
2019-03-28 10:58:43 +0100

    push day02

--- day02/ex03/who.php
+++ day02/ex03/who.php
@@ -1,6 +1,14 @@
 #!/usr/bin/php
 <?php
-$binaryData = file_get_contents("/var/run/utmpx");
-$output[] = unpack("A256user/A4id/A32tty/Ipid/Itype/Ltime/Lother/A256host/A64pad", $binaryData);
-print_r($output);
+$data = fopen("/var/run/utmpx", "r");
+while ($binary = fread($data, 628)) {
+	$arr[] = unpack("A256user/A4id/A32tty/Ipid/Itype/Ltime/Lother/A256host/A64pad", $binary);
+}
+sort($arr);
+date_default_timezone_set("Europe/Paris");
+foreach($arr as $elem) {
+    if ($elem['user'] !== "utmpx-1.00" && $elem['user'] != null && $elem['type'] == 7) {
+		echo $elem['user'] . " " . $elem['tty'] . "  " . date("M j H:i", $elem['time']) . " \n";
+    }
+}
 ?>
--- day02/ex06/srt.php
+++ day02/ex06/srt.php
@@ -1,8 +1,28 @@
 #!/usr/bin/php
 <?php
-if ($argc != 2 || !file_exists($argv[1])) {
-    exit();
+if ($argc == 1 || !file_exists($argv[1])) {
+	exit;
 }
-$file = fopen($argv[1], "r");
-
+$fd = fopen($argv[1], "r");
+while ($line = fgets($fd)) {
+	if (preg_match("~-->~", $line)) {
+		while (($lol = fgets($fd)) && $lol[0] != "\n") {
+			$srt .= $lol . "\n";
+		}
+		$arr[$line] = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $srt);;
+		unset($srt);
+	}
+}
+ksort($arr);
+$counter = 1;
+foreach($arr as $time => $srt) {
+	if ($counter != 1) {
+		echo "\n";
+	}
+	echo $counter . "\n";
+	echo $time;
+	echo $srt;
+	$counter++;
+}
+fclose($fd);
 ?>
